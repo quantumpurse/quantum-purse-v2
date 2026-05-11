718e1603c73ba5a71e79f7af56e5d8b91f8272be
tea2x <copperheart05@gmail.com>
2026-05-11 22:00:46 +0700

    fix the Windows pinentry build to link -lws2_32 and adjust ci workflow

--- vendor/build-pinentry.sh
+++ vendor/build-pinentry.sh
@@ -159,6 +159,7 @@ case "$OS" in
 
 	MINGW*|MSYS*)
 		echo "    Configuring for Windows (pinentry-w32)..."
+		export LDFLAGS="${LDFLAGS:-} -lws2_32"
 		./configure \
 			--disable-pinentry-curses \
 			--disable-fallback-curses \
