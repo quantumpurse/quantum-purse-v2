3bed6ccf9895e09a981eab9d29669629c0135601
tea2x <copperheart05@gmail.com>
2026-05-19 09:13:40 +0700

    exclude sw backed key management

--- crates/keychain/src/lib.rs
+++ crates/keychain/src/lib.rs
@@ -24,16 +24,10 @@ pub use macos::{delete_key, retrieve_key, store_key};
 #[cfg(target_os = "windows")]
 mod windows_hello;
 #[cfg(target_os = "windows")]
-#[allow(dead_code)]
-mod windows_dpapi;
-#[cfg(target_os = "windows")]
 pub use windows_hello::{delete_key, retrieve_key, store_key};
 
 #[cfg(target_os = "linux")]
 mod linux_tpm;
-#[cfg(target_os = "linux")]
-#[allow(dead_code)]
-mod linux_secret_service;
 #[cfg(target_os = "linux")]
 pub use linux_tpm::{delete_key, retrieve_key, store_key};
 
